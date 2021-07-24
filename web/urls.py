993e4101b4a0ead8add2b3ecc0c61c31e511ae58
blackfeather <304429477@qq.com>
2021-07-24 09:36:18 +0800

    '管理页面完成'

--- web/urls.py
+++ web/urls.py
@@ -4,7 +4,7 @@
 from django.conf.urls import url,include
 from web.views import account
 from web.views  import home
-from web.views  import project
+from web.views  import project,manage
 
 urlpatterns = [
     url(r'^register/$', account.register, name='register'),
@@ -15,9 +15,19 @@ urlpatterns = [
     url(r'^index/$', home.index, name='index'),
     url(r'^logout/$', account.logout, name='logout'),
 
-    #项目管理
+    #项目列表
     url(r'^project/list/$', project.project_list, name='project_list'),
     url(r'^project/star/(?P<project_type>\w+)/(?P<project_id>\d+)/$', project.project_star, name='project_star'),
     url(r'^project/unstar/(?P<project_type>\w+)/(?P<project_id>\d+)/$', project.project_unstar, name='project_unstar'),
+
+    #项目管理
+    url(r'^manage/(?P<project_id>\d+)/', include([
+        url(r'^dashboard/$', manage.dashboard, name='dashboard'),
+        url(r'^issues/$', manage.issues, name='issues'),
+        url(r'^statistics/$', manage.statistics, name='statistics'),
+        url(r'^file/$', manage.file, name='file'),
+        url(r'^wiki/$', manage.wiki, name='wiki'),
+        url(r'^setting/$', manage.setting, name='setting'),
+    ], None, None)),
 
 ]
---
+++ web/views/manage.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:blackfeather
+
+from django.shortcuts import render
+
+
+def dashboard(request, project_id):
+    return render(request, 'dashboard.html')
+
+
+def issues(request, project_id):
+    return render(request, 'issues.html')
+
+
+def statistics(request, project_id):
+    return render(request, 'statistics.html')
+
+
+def file(request, project_id):
+    return render(request, 'file.html')
+
+
+def wiki(request, project_id):
+    return render(request, 'wiki.html')
+
+
+def setting(request, project_id):
+    return render(request, 'setting.html')
+
